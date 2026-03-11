2eb0e454f4e258d5de00be905e8682c6570f3577
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-03-11 22:57:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int kthSmallest(vector<int> &arr, int k) {
+        int n = arr.size();
+        
+        priority_queue<int> pq;
+        
+        for (int i = 0; i<k; i++) {
+            pq.push(arr[i]);
+        }
+        
+        for (int i = k; i<n; i++) {
+            if (pq.top() > arr[i]) {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        return pq.top();
+    }
+};
